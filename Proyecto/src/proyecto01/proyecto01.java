db656f50eb006f59a432326ad766dcd71dfb74d3
AlejandroLLancafil <AlejandroLLancafil@Alejandro>
2014-09-15 17:32:52 -0400

    primera version

---
+++ Proyecto/src/proyecto01/proyecto01.java
@@ -0,0 +1,30 @@
+package proyecto01;
+
+public class proyecto01 {
+
+	public static void main(String[] args) {
+
+		
+	}
+	
+	public static String[] productos() {
+
+        String[] productos = {"Leche", "Manjar", "Nectar", "Atun", "Arroz", "Fideos",
+            "Mayonesa", "Bebidas", "Aceite", "Vinagre"};
+        
+        return productos;
+    }
+
+    public static String[] supermercados() {
+
+        String[] supermercados = {"Lider", "Trebol", "Santa Isabel", "Mayorista 10",
+            "Unimarc", "Jumbo", "Acuenta", "Alvi"};
+        return supermercados;
+    }
+
+    public static void precio(String[] prod, String[] sup) {
+        
+        
+    }
+
+}
